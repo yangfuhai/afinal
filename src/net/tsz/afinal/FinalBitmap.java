1251fd2a674b4711a1797fc276ac26c2538ac747
michael <michael@tsz.net>
2012-10-30 18:33:52 +0800

    change copyright

--- src/net/tsz/afinal/FinalBitmap.java
+++ src/net/tsz/afinal/FinalBitmap.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
+/**
+ * Copyright (c) 2012-2013, Michael Yang Ñî¸£º£ (www.yangfuhai.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package net.tsz.afinal;
 
 import android.content.Context;
