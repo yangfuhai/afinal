d9d9398acbb801e676464134c652d912a7f39e1b
yangfuhai <yfh1988@gmail.com>
2013-04-12 12:25:46 +0800

    完善finalDb的异常信息

--- src/net/tsz/afinal/exception/AfinalException.java
+++ src/net/tsz/afinal/exception/AfinalException.java
@@ -17,5 +17,21 @@ package net.tsz.afinal.exception;
 
 public class AfinalException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
+	
+	public AfinalException() {
+		super();
+	}
+	
+	public AfinalException(String msg) {
+		super(msg);
+	}
+	
+	public AfinalException(Throwable ex) {
+		super(ex);
+	}
+	
+	public AfinalException(String msg,Throwable ex) {
+		super(msg,ex);
+	}
 
 }
