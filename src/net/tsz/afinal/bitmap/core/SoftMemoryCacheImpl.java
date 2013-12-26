7626cba5bc224d7b4c06d7fecaa310dc10a916ee
lsjwzh <lsjwzh@163.com>
2013-12-26 11:26:28 +0800

    恢复更改

--- src/net/tsz/afinal/bitmap/core/SoftMemoryCacheImpl.java
+++ src/net/tsz/afinal/bitmap/core/SoftMemoryCacheImpl.java
@@ -16,41 +16,49 @@
 package net.tsz.afinal.bitmap.core;
 
 import java.lang.ref.SoftReference;
-import java.util.HashMap;
+
+import net.tsz.afinal.utils.Utils;
 
 import android.graphics.Bitmap;
 
 public class SoftMemoryCacheImpl implements IMemoryCache {
 
-	private final HashMap<String, SoftReference<Bitmap>> mMemoryCache;
-	
-	public SoftMemoryCacheImpl(int size) {
-		
-		mMemoryCache = new HashMap<String, SoftReference<Bitmap>>();
-	}
-	
-	@Override
-	public void put(String key, Bitmap bitmap) {
-		mMemoryCache.put(key, new SoftReference<Bitmap>(bitmap));
-	}
+    private final LruMemoryCache<String, SoftReference<Bitmap>> mMemoryCache;
 
-	@Override
-	public Bitmap get(String key) {
-		 SoftReference<Bitmap> memBitmap = mMemoryCache.get(key);
-         if(memBitmap!=null){
-         	return memBitmap.get();
-         }
-         return null;
-	}
+    public SoftMemoryCacheImpl(int size) {
+        mMemoryCache = new LruMemoryCache<String, SoftReference<Bitmap>>(size) {
+            @Override
+            protected int sizeOf(String key, SoftReference<Bitmap> sBitmap) {
+                final Bitmap bitmap = sBitmap==null ? null : sBitmap.get();
+                if(bitmap == null)
+                    return 1;
+                return Utils.getBitmapSize(bitmap);
+            }
+        };
+    }
 
-	@Override
-	public void evictAll() {
-		mMemoryCache.clear();
-	}
+    @Override
+    public void put(String key, Bitmap bitmap) {
+        mMemoryCache.put(key, new SoftReference<Bitmap>(bitmap));
+    }
 
-	@Override
-	public void remove(String key) {
-		mMemoryCache.remove(key);
-	}
+    @Override
+    public Bitmap get(String key) {
+        SoftReference<Bitmap> memBitmap = mMemoryCache.get(key);
+        if(memBitmap!=null){
+            return memBitmap.get();
+        }
+        return null;
+    }
+
+    @Override
+    public void evictAll() {
+        mMemoryCache.evictAll();
+    }
+
+    @Override
+    public void remove(String key) {
+        mMemoryCache.remove(key);
+    }
 
 }
