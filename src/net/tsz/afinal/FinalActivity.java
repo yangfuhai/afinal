c8fb67a46346be68640b78c746b3d462d180f3fa
wangzengyang <ngwang.io@gmail.com>
2014-08-05 16:23:39 +0800

    增加对继承树中所有类(除java/android系统类)所有元素的inject.

--- src/net/tsz/afinal/FinalActivity.java
+++ src/net/tsz/afinal/FinalActivity.java
@@ -23,112 +23,125 @@ import net.tsz.afinal.annotation.view.ViewInject;
 import android.app.Activity;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.AbsListView;
 
 public abstract class FinalActivity extends Activity {
 
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        initInjectedView(this);
+    }
 
-	public void setContentView(int layoutResID) {
-		super.setContentView(layoutResID);
-		initInjectedView(this);
-	}
+    public void setContentView(View view, LayoutParams params) {
+        super.setContentView(view, params);
+        initInjectedView(this);
+    }
 
+    public void setContentView(View view) {
+        super.setContentView(view);
+        initInjectedView(this);
+    }
 
-	public void setContentView(View view, LayoutParams params) {
-		super.setContentView(view, params);
-		initInjectedView(this);
-	}
+    public static void initInjectedView(Activity activity) {
+        initInjectedView(activity, activity.getWindow().getDecorView());
+    }
 
+    public static void initInjectedView(Object injectedSource, View sourceView) {
+        initInjectedWithBaseView(injectedSource, sourceView, injectedSource.getClass());
+    }
 
-	public void setContentView(View view) {
-		super.setContentView(view);
-		initInjectedView(this);
-	}
-	
+    public static void initInjectedWithBaseView(Object injectedSource, View sourceView, Class<?> clazz) {
+        Class<?> superClass = clazz.getSuperclass();
+        if (shouldInject(superClass))
+            initInjectedWithBaseView(injectedSource, sourceView, superClass);
+        inject(injectedSource, sourceView, clazz);
+    }
 
-	public static void initInjectedView(Activity activity){
-		initInjectedView(activity, activity.getWindow().getDecorView());
-	}
-	
-	
-	public static void initInjectedView(Object injectedSource,View sourceView){
-		Field[] fields = injectedSource.getClass().getDeclaredFields();
-		if(fields!=null && fields.length>0){
-			for(Field field : fields){
-				try {
-					field.setAccessible(true);
-					
-					if(field.get(injectedSource)!= null )
-						continue;
-				
-					ViewInject viewInject = field.getAnnotation(ViewInject.class);
-					if(viewInject!=null){
-						
-						int viewId = viewInject.id();
-					    field.set(injectedSource,sourceView.findViewById(viewId));
-					
-					    setListener(injectedSource,field,viewInject.click(),Method.Click);
-						setListener(injectedSource,field,viewInject.longClick(),Method.LongClick);
-						setListener(injectedSource,field,viewInject.itemClick(),Method.ItemClick);
-						setListener(injectedSource,field,viewInject.itemLongClick(),Method.itemLongClick);
-						
-						Select select = viewInject.select();
-						if(!TextUtils.isEmpty(select.selected())){
-							setViewSelectListener(injectedSource,field,select.selected(),select.noSelected());
-						}
-						
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-	
-	
-	private static void setViewSelectListener(Object injectedSource,Field field,String select,String noSelect)throws Exception{
-		Object obj = field.get(injectedSource);
-		if(obj instanceof View){
-			((AbsListView)obj).setOnItemSelectedListener(new EventListener(injectedSource).select(select).noSelect(noSelect));
-		}
-	}
-	
-	
-	private static void setListener(Object injectedSource,Field field,String methodName,Method method)throws Exception{
-		if(methodName == null || methodName.trim().length() == 0)
-			return;
-		
-		Object obj = field.get(injectedSource);
-		
-		switch (method) {
-			case Click:
-				if(obj instanceof View){
-					((View)obj).setOnClickListener(new EventListener(injectedSource).click(methodName));
-				}
-				break;
-			case ItemClick:
-				if(obj instanceof AbsListView){
-					((AbsListView)obj).setOnItemClickListener(new EventListener(injectedSource).itemClick(methodName));
-				}
-				break;
-			case LongClick:
-				if(obj instanceof View){
-					((View)obj).setOnLongClickListener(new EventListener(injectedSource).longClick(methodName));
-				}
-				break;
-			case itemLongClick:
-				if(obj instanceof AbsListView){
-					((AbsListView)obj).setOnItemLongClickListener(new EventListener(injectedSource).itemLongClick(methodName));
-				}
-				break;
-			default:
-				break;
-		}
-	}
-	
-	public enum Method{
-		Click,LongClick,ItemClick,itemLongClick
-	}
-	
+    private static boolean shouldInject(Class<?> clazz) {
+        String packageName = clazz.getPackage().getName();
+        return !(packageName.startsWith("java") || packageName.startsWith("android") || packageName.startsWith("com.android") || packageName
+                .startsWith("com.google"));
+    }
+
+    private static void inject(Object injectedSource, View sourceView, Class<?> clazz) {
+        Field[] fields = clazz.getDeclaredFields();
+        if (fields != null && fields.length > 0) {
+            for (Field field : fields) {
+                try {
+                    field.setAccessible(true);
+                    ViewInject viewInject = field.getAnnotation(ViewInject.class);
+                    View valueView = null;
+                    Object valueObject = null;
+                    if (viewInject != null) {
+                        int viewId = viewInject.id();
+                        valueView = sourceView.findViewById(viewId);
+                        valueObject = field.get(injectedSource);
+                        if (valueObject != null && valueObject == valueView)
+                            continue;
+                        field.set(injectedSource, valueView);
+
+                        setListener(injectedSource, field, viewInject.click(), Method.Click);
+                        setListener(injectedSource, field, viewInject.longClick(), Method.LongClick);
+                        setListener(injectedSource, field, viewInject.itemClick(), Method.ItemClick);
+                        setListener(injectedSource, field, viewInject.itemLongClick(), Method.itemLongClick);
+
+                        Select select = viewInject.select();
+                        if (!TextUtils.isEmpty(select.selected())) {
+                            setViewSelectListener(injectedSource, field, select.selected(), select.noSelected());
+                        }
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static void setViewSelectListener(Object injectedSource, Field field, String select, String noSelect) throws Exception {
+        Object obj = field.get(injectedSource);
+        if (obj instanceof View) {
+            ((AbsListView) obj).setOnItemSelectedListener(new EventListener(injectedSource).select(select).noSelect(noSelect));
+        }
+    }
+
+    private static void setListener(Object injectedSource, Field field, String methodName, Method method) throws Exception {
+        if (methodName == null || methodName.trim().length() == 0)
+            return;
+
+        Object obj = field.get(injectedSource);
+
+        switch (method) {
+        case Click:
+            if (obj instanceof View) {
+                OnClickListener l = new EventListener(injectedSource).click(methodName);
+                ((View) obj).setOnClickListener(l);
+            }
+            break;
+        case ItemClick:
+            if (obj instanceof AbsListView) {
+                ((AbsListView) obj).setOnItemClickListener(new EventListener(injectedSource).itemClick(methodName));
+            }
+            break;
+        case LongClick:
+            if (obj instanceof View) {
+                ((View) obj).setOnLongClickListener(new EventListener(injectedSource).longClick(methodName));
+            }
+            break;
+        case itemLongClick:
+            if (obj instanceof AbsListView) {
+                ((AbsListView) obj).setOnItemLongClickListener(new EventListener(injectedSource).itemLongClick(methodName));
+            }
+            break;
+        default:
+            break;
+        }
+    }
+
+    public enum Method {
+        Click, LongClick, ItemClick, itemLongClick
+    }
+
 }
